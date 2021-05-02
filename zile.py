f8fb9de99a43b8b81c07c558122e772dcdfb932b
abdallaEG <52584554+alanEG@users.noreply.github.com>
2021-05-02 22:43:33 +0200

    Update zile.py

--- zile.py
+++ zile.py
@@ -75,7 +75,7 @@ def fromUrl(url):
 args = list(sys.argv)[1:]
 if "--file" in args: # if file parameter has given as argument
     totalFiles = []
-    filesn = sys.argv[2]
+    filesn = list(sys.argv)[2]
     for root, dirs, files in os.walk(filesn):
         tempFiles = [os.path.join(os.getcwd(),os.path.join(root, i)[2:]) for i in files] # find every file under current directory
         totalFiles+=tempFiles # and add them to totalFiles array
